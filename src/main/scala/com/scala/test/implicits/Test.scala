68a3d51236fba2ba8d975028176882b2e0063400
zhaojuno <zhaojuno@enn.cn>
2018-05-09 16:53:56 +0800

    add implicit

---
+++ src/main/scala/com/scala/test/implicits/Test.scala
@@ -0,0 +1,34 @@
+package com.scala.test.implicits
+
+/**
+  * Created by jufeng on 2018/5/9.
+  *
+  *
+    scala > def calcTax(amount: Float)(implicit rate: Float): Float = amount * rate
+    scala > implicit val currentTaxRate = 0.08F
+    scala > val tax = calcTax(50000F) // 4000.0
+
+
+  scala> val i: Int = 3.5 //直接报错
+  加上这句：
+  scala> implicit def double2Int(d: Double) = d.toInt
+  再运行，没报错
+  scala> val i: Int = 3.5  //i=3
+  */
+class Test {
+
+}
+
+class SwingType{
+  def  wantLearned(sw : String) = println("兔子已经学会了"+sw)
+}
+object swimming{
+  implicit def getType(s : AminalType) = new SwingType
+}
+class AminalType
+
+object AminalType extends  App{
+  import  swimming._
+  val rabbit = new AminalType
+  rabbit.wantLearned("breaststroke")         //蛙泳
+}
