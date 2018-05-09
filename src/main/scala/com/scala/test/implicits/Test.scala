11aba74de4c7b91a42d4791e865d4af5ffe37dde
zhaojuno <zhaojuno@enn.cn>
2018-05-09 17:02:23 +0800

    update some

--- src/main/scala/com/scala/test/implicits/Test.scala
+++ src/main/scala/com/scala/test/implicits/Test.scala
@@ -3,12 +3,12 @@ package com.scala.test.implicits
 /**
   * Created by jufeng on 2018/5/9.
   *
-  *
+  *隐式参数
     scala > def calcTax(amount: Float)(implicit rate: Float): Float = amount * rate
     scala > implicit val currentTaxRate = 0.08F
     scala > val tax = calcTax(50000F) // 4000.0
 
-
+隐式地转换类型
   scala> val i: Int = 3.5 //直接报错
   加上这句：
   scala> implicit def double2Int(d: Double) = d.toInt
@@ -16,7 +16,7 @@ package com.scala.test.implicits
   scala> val i: Int = 3.5  //i=3
   */
 
-
+//隐式调用函数
 class SwingType{
   def  wantLearned(sw : String) = println("兔子已经学会了 "+sw)
 }
